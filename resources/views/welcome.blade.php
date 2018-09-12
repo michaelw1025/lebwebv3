e037c73e2539787d2d9ede2af48d5dd4ae1f30bd
Mike Williamson <michaelw1025@gmail.com>
2018-09-12 01:01:44 -0500

    tweeks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.guest-app')
 
 @section('content')
-    
+<div class="d-flex flex-column w-100">
+    <div class="jumbotron jumbotron-fluid my-auto text-center bg-white">
+        <p class="shadow-lg">Welcome To <span class="text-primary">LebWeb</span></p>
+    </div>
+</div>
 @endsection
